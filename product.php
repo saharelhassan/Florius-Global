7121970be5ec803c93d7f73a6f79037ecc17b0bd
saharelhassan <sahar_elhassan@outlook.com>
2018-01-12 15:13:03 +0400

    Add product.php

--- product.php
+++ product.php
@@ -1,11 +1,15 @@
 <?php
  class Product {
-
-	private $p = 2.5;
-	private $dimension1 = 0;
-    private $dimension2 = 0;
-	private $area = 0;
-	private $shape = null;
+	 // the constant number $2.5 
+	 private $p = 2.5;
+	 // First dimension
+	 private $dimension1 = 0;
+	 // Second dimension
+	 private $dimension2 = 0;
+	 // Shape's area
+	 private $area = 0;
+	 // Shape type
+	 private $shape = null;
 
 	function __construct($shape, $dimension1, $dimension2) {
         
@@ -15,6 +19,7 @@
 
     }
 
+	// function to calculate the price for the product based on the chosen shape and the giving dimensions
 	function price()
 	{
 		switch ($this->shape) {
@@ -31,7 +36,7 @@
 				$this->area = ($this->dimension1 * $this->dimension2) / 2;
 			break;
 		}
-		
+		// the price of a product is calculated as $2.5 * area of the shape 
 		return $this->p * $this->area;
 	}
 }
